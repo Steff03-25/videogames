c2b1cd5e20fa25ae9834027a3d13a03a3e64a988
Stefanie <stefanycas2025@gmail.com>
2024-09-13 21:35:12 -0500

    number race

---
+++ Scripts Python/number_race/main.py
@@ -0,0 +1,13 @@
+'''
+Script description: Number race
+Dev: Stefanie Castillo
+Date: 13-09-2024
+'''
+
+from random import randint
+
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+print(f"Dice 1:{dice1}")
+print(f"Dice 2:{dice2}")
